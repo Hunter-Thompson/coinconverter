ee43dd6df82b8c99631a61c15f060c8ce3b1a74c
Aatman bos <aatman@expensebit.com>
2020-03-06 13:39:00 +0530

    made it cleaner and smarter, also added converter

--- index.js
+++ index.js
@@ -11,23 +11,11 @@ var app = express();
 
 
 
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////
-
-
-app.get('/coin', function(req, res){
+app.get('/api/price', function(req, res){
   var coindata = req.query.id;
-  let from = coindata.replace(/\?.+/g,"$'")
-  let to = coindata.split("?").pop();
+  console.log(coindata)
+  let from = coindata[0]
+  let to   = coindata[1]
 
 
   let op = {
@@ -42,8 +30,7 @@ app.get('/coin', function(req, res){
             let converted = f + ''
             let changed = converted.split(":").pop()
             let final = changed.slice(0, -1)
-            console.log(2.3*final)
-            res.send("1 " + from + " = " + to + ' ' + final + "\n")
+            res.send("1 " + from + " = " + final + ' ' + to + "\n")
           } catch(e) {
               console.error(e);
           }
@@ -56,11 +43,46 @@ app.get('/coin', function(req, res){
 
 //////////////////////////////////////////////////////////////////////////////
 
-app.get('/fullcoin', function(req, res){
-  var coindata = req.query.id;
-  let from = coindata.replace(/\?.+/g,"$'")
-  let to = coindata.split("?").pop();
 
+app.get('/api/converter', function(req, res){
+  var coindata = req.query.id;
+  console.log(coindata)
+  let from = coindata[0]
+  let to   = coindata[1]
+  let num = coindata[2]
+
+
+  let op = {
+    host: "min-api.cryptocompare.com",
+    port: 443,
+    path: `/data/price?fsym=${from}&tsyms=${to}`
+  }  
+
+  let request = http.request(op, x=> {
+      x.on("data", f => {
+          try{
+            let price = f + ''
+            let changed = price.split(":").pop()
+            let final = changed.slice(0, -1)
+            let converted = num*final
+            console.log(converted)
+            res.send(num + ' ' + from + ' = ' + converted + ' ' + to + `\n`)
+          } catch(e) {
+              console.error(e);
+          }
+      })
+  })
+  request.on("error", console.error);
+  request.end()
+});
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+app.get('/api/fullcoin', function(req, res){
+  var coindata = req.query.id;
+  let from = coindata[0]
+  let to   = coindata[1]
 
   let op = {
     host: "min-api.cryptocompare.com",
